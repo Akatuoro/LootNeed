dd32b52946c3f231e21c35dc3e063e93a16c3413
Akatuoro <akatuoro6@gmail.com>
2015-10-07 12:15:12 +0200

    Spielwiese für Ben hinzugefügt

---
+++ looting.lua
@@ -0,0 +1,16 @@
+-- Für Ben zum bearbeiten
+
+local addon, ns = ...	-- addon ist der Name des Addons, ns ist der 'namespace' (ns.lootliste z.B. kann somit auch aus anderen Dateien des Addons abgerufen werden)
+
+local eventFrameEvent = function(self, event, ...) -- ... sind die restlichen Parameter
+	if event == "PLAYER_STARTED_MOVING" then
+		-- code
+	end
+end
+
+-- Ein Frame, um Events registrieren zu können:
+ns.eventFrame = CreateFrame("Frame", addon .. "EventFrame", UIParent)
+-- Registrieren eines Events:
+ns.eventFrame:RegisterEvent("PLAYER_STARTED_MOVING")
+-- Füttern der Funktion, wenn das Event passiert:
+ns.eventFrame:OnScript("OnEvent", eventFrameEvent)
